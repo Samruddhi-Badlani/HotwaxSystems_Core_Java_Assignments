17adff73fc7ad683ca6fd565265dda5885ff6714
Samruddhi-Badlani <samruddhi4227132@gmail.com>
2021-07-12 19:28:04 +0530

    Read words from the file

--- Assignment_1/src/Main.java
+++ Assignment_1/src/Main.java
@@ -2,11 +2,34 @@ import java.util.ArrayList;
 
 public class Main {
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args)  {
 		// TODO Auto-generated method stub
+		//Creating an object of the class 
 		URL_Content_Word_Counter myCounter = new URL_Content_Word_Counter();
-		ArrayList<String> resultArrayList= myCounter.read_urls_from_file();
-
+		
+		//Reading URLS from the file
+		ArrayList<String> my_url_list= myCounter.read_urls_from_file();
+		if(my_url_list!=null) {
+			for(String urlString : my_url_list) {
+				System.out.println(urlString);
+			}
+		}
+		else {
+			System.out.println("Error in reading URL files");
+			return;
+		}
+		
+		//Reading URLS from the file
+		ArrayList<String> my_word_list= myCounter.read_words_from_file();
+		if(my_word_list!=null) {
+			for(String wordString : my_word_list) {
+				System.out.println(wordString);
+			}
+		}
+		else {
+			System.out.println("Error in reading Words files");
+			return;
+		}
 	}
 
 }
--- Assignment_1/src/URL_Content_Word_Counter.java
+++ Assignment_1/src/URL_Content_Word_Counter.java
@@ -2,14 +2,53 @@ import java.net.*;
 import java.io.*;
 import java.util.ArrayList;
 public class URL_Content_Word_Counter {
-	public ArrayList<String> read_urls_from_file() throws Exception{
-		ArrayList<String> list_of_file = new ArrayList<String>();
-		File file1 = new File("src/URLS.txt");
-		BufferedReader br = new BufferedReader(new FileReader(file1));
-		  
-		  String st;
-		  while ((st = br.readLine()) != null)
-		    System.out.println(st);
-		return list_of_file;
+	public ArrayList<String> read_urls_from_file() {
+		try {
+			ArrayList<String> list_of_file = new ArrayList<String>();
+			File file1 = new File("src/URLS");
+			BufferedReader br = new BufferedReader(new FileReader(file1));
+			String st;
+			while ((st = br.readLine()) != null) {
+			    list_of_file.add(st);
+			}
+			return list_of_file;
+		}
+		catch (FileNotFoundException exception) {
+			// TODO: handle exception
+			exception.printStackTrace();
+			return null;
+		}
+		catch (IOException exception) {
+			// TODO Auto-generated catch block
+			exception.printStackTrace();
+			return null;
+		}
+	
+		
+	}
+	
+	public ArrayList<String> read_words_from_file() {
+		try {
+			ArrayList<String> list_of_words = new ArrayList<String>();
+			File file1 = new File("src/Words");
+			BufferedReader br = new BufferedReader(new FileReader(file1));
+			String st;
+			while ((st = br.readLine()) != null) {
+			    list_of_words.add(st);
+			}
+			return list_of_words;
+		}
+		catch (FileNotFoundException exception) {
+			// TODO: handle exception
+			exception.printStackTrace();
+			return null;
+		}
+		catch (IOException exception) {
+			// TODO Auto-generated catch block
+			exception.printStackTrace();
+			return null;
+		}
+	
+		
 	}
 }
