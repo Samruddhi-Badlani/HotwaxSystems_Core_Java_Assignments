d3bc8cb5585244661fb40db18702d0b26e988d40
Samruddhi-Badlani <samruddhi4227132@gmail.com>
2021-07-12 23:53:05 +0530

    Added a function to process all the URLS
    A function to read all the URLS from the file , read all the words from
    the file and to find the frequency of each word in the web content that
    uses the previous function written
    
    Tested the above function in the main class

--- Assignment_1/src/Main.java
+++ Assignment_1/src/Main.java
@@ -1,40 +1,20 @@
 import java.util.ArrayList;
-
+import java.util.Map;
 public class Main {
 
 	public static void main(String[] args)  {
 		// TODO Auto-generated method stub
 		//Creating an object of the class 
 		URL_Content_Word_Counter myCounter = new URL_Content_Word_Counter();
+		Map<String, Map<String, Integer>> resultMap = myCounter.process_all_urls();
 		
-		//Reading URLS from the file
-		ArrayList<String> my_url_list= myCounter.read_urls_from_file();
-		if(my_url_list!=null) {
-			for(String urlString : my_url_list) {
-				System.out.println(urlString);
+		for(Map.Entry<String, Map<String, Integer>> my_Map : resultMap.entrySet()) {
+			System.out.println(my_Map.getKey());
+			Map<String, Integer> word_map = my_Map.getValue();
+			for(Map.Entry<String, Integer> wordEntry : word_map.entrySet()) {
+				System.out.println(wordEntry.getKey()+" "+wordEntry.getValue());
 			}
 		}
-		else {
-			System.out.println("Error in reading URL files");
-			return;
-		}
-		
-		//Reading Words from the file
-		ArrayList<String> my_word_list= myCounter.read_words_from_file();
-		if(my_word_list!=null) {
-			for(String wordString : my_word_list) {
-				System.out.println(wordString);
-			}
-		}
-		else {
-			System.out.println("Error in reading Words files");
-			return;
-		}
-		
-		ArrayList<String>my_web_content = myCounter.content_of_url("https://www.google.com");
-		int my_word_count  = myCounter.get_my_count("google", my_web_content);
-		
-		System.out.println(my_word_count);
 	}
 
 }
--- Assignment_1/src/URL_Content_Word_Counter.java
+++ Assignment_1/src/URL_Content_Word_Counter.java
@@ -1,6 +1,10 @@
 import java.net.*;
 import java.io.*;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.activation.MailcapCommandMap;
 public class URL_Content_Word_Counter {
 	
 	public ArrayList<String> read_urls_from_file() {
@@ -96,4 +100,35 @@ public class URL_Content_Word_Counter {
 		return frequency;
 	}
 	
+	// Get the word count for each word in a file for a particular URL
+	public Map<String,Integer> process_one_url(String url){
+		
+		Map<String, Integer> word_frequency_countMap = new HashMap<String, Integer>();
+		ArrayList<String> content = content_of_url(url);
+		ArrayList<String> my_words = read_words_from_file();
+		for(String word : my_words) {
+			int count = get_my_count(word,content);
+			word_frequency_countMap.put(word, count);
+		}
+	return word_frequency_countMap;		
+	}
+	
+	//Process all the URLS 
+	public Map<String,Map<String, Integer>> process_all_urls(){
+		
+		Map<String, Map<String, Integer>> url_map = new HashMap<String,Map<String, Integer>>();
+		
+		ArrayList<String> my_urls = read_urls_from_file();
+		for(String url : my_urls) {
+			ArrayList<String> content = content_of_url(url);
+			ArrayList<String> my_words = read_words_from_file();
+			Map<String, Integer> word_frequency_countMap = new HashMap<String, Integer>();
+			for(String word : my_words) {
+				int count = get_my_count(word,content);
+				word_frequency_countMap.put(word, count);
+			}
+			url_map.put(url, word_frequency_countMap);
+		}
+	return url_map;		
+	}
 }
