50391a3ad4d673ee0d13c360bd755e046949d491
Samruddhi-Badlani <samruddhi4227132@gmail.com>
2021-07-13 01:02:17 +0530

    Formated the output as per the question

--- Assignment_1/src/Main.java
+++ Assignment_1/src/Main.java
@@ -7,13 +7,25 @@ public class Main {
 		//Creating an object of the class 
 		URL_Content_Word_Counter myCounter = new URL_Content_Word_Counter();
 		Map<String, Map<String, Integer>> resultMap = myCounter.process_all_urls();
-		
+		System.out.println("OUTPUT #1\n ========\n");
 		for(Map.Entry<String, Map<String, Integer>> my_Map : resultMap.entrySet()) {
 			System.out.println(my_Map.getKey());
+			int top_words_count =3;
+			int count_words=0;
 			Map<String, Integer> word_map = my_Map.getValue();
 			for(Map.Entry<String, Integer> wordEntry : word_map.entrySet()) {
+				count_words++;
+				if(count_words > top_words_count) {
+					break;
+				}
 				System.out.println(wordEntry.getKey()+" "+wordEntry.getValue());
 			}
+		}
+		System.out.println("==============================");
+		System.out.println("OUTPUT #2");
+		myCounter.my_output_for_each_word=myCounter.sortByValue(myCounter.my_output_for_each_word);
+		for(Map.Entry<String, Integer> entry:myCounter.my_output_for_each_word.entrySet()) {
+			System.out.println(entry.getKey()+" "+entry.getValue());
 		}
 	}
 
--- Assignment_1/src/URL_Content_Word_Counter.java
+++ Assignment_1/src/URL_Content_Word_Counter.java
@@ -1,11 +1,26 @@
 import java.net.*;
+import java.util.Comparator;
+import java.util.TreeMap;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.Collections;
+import java.awt.Choice;
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import javax.activation.MailcapCommandMap;
 public class URL_Content_Word_Counter {
+	public Map<String, Integer> my_output_for_each_word;
+	public URL_Content_Word_Counter(){
+		my_output_for_each_word=new HashMap<String, Integer>();
+	}
 	
 	public ArrayList<String> read_urls_from_file() {
 		
@@ -43,6 +58,7 @@ public class URL_Content_Word_Counter {
 			String st;
 			while ((st = br.readLine()) != null) {
 			    list_of_words.add(st);
+			    
 			}
 			br.close();
 			return list_of_words;
@@ -95,6 +111,7 @@ public class URL_Content_Word_Counter {
 		for(String line : content ) {
 			if(line.contains(word)) {
 				frequency++;
+				
 			}
 		}
 		return frequency;
@@ -109,8 +126,38 @@ public class URL_Content_Word_Counter {
 		for(String word : my_words) {
 			int count = get_my_count(word,content);
 			word_frequency_countMap.put(word, count);
+			
 		}
 	return word_frequency_countMap;		
+	}
+	
+	public Map<String, Integer> sortByValue(Map<String, Integer> map) {
+	
+		//convert HashMap into List   
+		boolean order=false;
+		List<Entry<String, Integer>> list = new LinkedList<Entry<String, Integer>>(map.entrySet());  
+		//sorting the list elements  
+		Collections.sort(list, new Comparator<Entry<String, Integer>>()   
+		{  
+		public int compare(Entry<String, Integer> o1, Entry<String, Integer> o2)   
+		{  
+		if (order)   
+		{  
+		//compare two object and return an integer  
+		return o1.getValue().compareTo(o2.getValue());}   
+		else   
+		{  
+		return o2.getValue().compareTo(o1.getValue());  
+		}  
+		}  
+		});  
+		//prints the sorted HashMap  
+		Map<String, Integer> sortedMap = new LinkedHashMap<String, Integer>();  
+		for (Entry<String, Integer> entry : list)   
+		{  
+		sortedMap.put(entry.getKey(), entry.getValue());  
+		}  
+		return sortedMap;  
 	}
 	
 	//Process all the URLS 
@@ -126,7 +173,15 @@ public class URL_Content_Word_Counter {
 			for(String word : my_words) {
 				int count = get_my_count(word,content);
 				word_frequency_countMap.put(word, count);
+				if(my_output_for_each_word.containsKey(word)) {
+					my_output_for_each_word.put(word, my_output_for_each_word.get(word)+count);
+					//System.out.println("hello");
+				}
+				else {
+					my_output_for_each_word.put(word,count);
+				}
 			}
+			word_frequency_countMap=sortByValue(word_frequency_countMap);
 			url_map.put(url, word_frequency_countMap);
 		}
 	return url_map;		
